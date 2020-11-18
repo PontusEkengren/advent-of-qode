4b6c2159f5d4db49dab35a8d37283fb79f47d76e
carlssonn <pontus_carlsson@hotmail.com>
2020-11-18 17:56:06 +0100

    Clickable by date

--- src/Tree.js
+++ src/Tree.js
@@ -3,21 +3,35 @@ import { Link } from './Styled/defaults';
 import { Branch } from './Styled/christmas.js';
 import { treeData } from './treeData.js';
 
+const getTreeData = () => {
+  const date = new Date().toISOString();
+  let month = date.slice(5, 7);
+  if (month === '11') {
+    let dayString = date.slice(8, 10);
+    const day = parseInt(dayString);
+
+    return treeData.map((branch) => {
+      return { ...branch, active: branch.day <= day };
+    });
+  }
+
+  return treeData;
+};
+
 const getBranchContent = (branch) => {
   return (
     <tr>
-      <td>
-        <Link href='http://google.se'>{branch.ornament}</Link>
-      </td>
+      <td>{branch.active ? <Link href='http://google.se'>{branch.ornament}</Link> : branch.ornament}</td>
       <td>{branch.day > 0 ? branch.day : ''}</td>
     </tr>
   );
 };
 
 export default function Tree() {
+  getTreeData();
   return (
     <div style={{ width: '950px', margin: '20px 0 0 40px' }}>
-      {treeData.map((branch, i) => (
+      {getTreeData().map((branch, i) => (
         <Branch active={branch.active} key={i}>
           <table>
             <tbody>{getBranchContent(branch)}</tbody>
